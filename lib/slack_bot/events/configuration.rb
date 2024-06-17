f0f46765d64ec65aaf76648b3f32a13a7d51a565
Matt Taylor <mattius.taylor@gmail.com>
2024-06-16 23:54:06 -0700

    Make it actually work now!

--- lib/slack_bot/events/configuration.rb
+++ lib/slack_bot/events/configuration.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "class_composer"
+require "logger"
 
 module SlackBot
   module Events
@@ -11,22 +12,59 @@ module SlackBot
       add_composer :client_signing_secret, allowed: String, default: ENV["SLACK_SIGNING_SECRET"]
       add_composer :client_socket_token, allowed: String, default: ENV["SLACK_SOCKET_TOKEN"]
       add_composer :client_verification_token, allowed: String, default: ENV["SLACK_VERIFICATION_TOKEN"]
+      add_composer :print_tldr, allowed: [true.class, false.class], default: true
 
-      def register_listener(name:, handler:)
-        @listeners ||= {}
-
-        if @listeners.has_key(name.to_sym)
+      def register_listener(name:, handler:, on_success: nil, on_failure: nil)
+        if event_handler = listeners[name.to_sym]
+          logger.warn "`#{name}` already exists as listener event. Reseting with new input"
         end
 
-        @listeners[name] = handler
+        validate_listener!(handler: handler, on_success: on_success, on_failure: on_failure)
 
+        listeners[name.to_sym] = { handler: handler, name: name, on_success: on_success, on_failure: on_failure }
         true
       end
 
       def remove_listener(name:)
+        listeners ||= {}
+      end
+
+      def logger
+        @logger ||= Logger.new(STDOUT)
+      end
+
+      def logger=(log)
+        @logger = log
+      end
+
+      def listeners
         @listeners ||= {}
+      end
 
+      private
 
+      def validate_listener!(handler:, on_success:, on_failure:)
+        unless retreive_method(object: handler, meth_name: :call)
+          raise ArgumentError, "When present, handler argument needs to respond_to :call with schema: and raw_data: kwargs"
+        end
+
+        if on_success
+          unless retreive_method(object: on_success, meth_name: :call)
+            raise ArgumentError, "When present, on_success argument needs to respond_to :call with 1 arguments passed in"
+          end
+        end
+
+        if on_failure
+          unless retreive_method(object: on_failure, meth_name: :call)
+            raise ArgumentError, "When present, on_failure argument needs to respond_to :call with 2 arguments passed in"
+          end
+        end
+      end
+
+      def retreive_method(object:, meth_name:)
+        object.method(meth_name.to_sym)
+      rescue NameError
+        nil
       end
     end
   end
--- lib/slack_bot/events/schemas/type/message.rb
+++ lib/slack_bot/events/schemas/type/message.rb
@@ -21,12 +21,29 @@ module SlackBot
 
           add_field name: :blocks, type: SlackBot::Events::Schemas::Type::Block, array_of_types: true
 
+          add_field name: :thread_ts, type: String, required: false
+          add_field name: :parent_user_id, type: String, required: false
+
           add_field name: :channel, type: String
           add_field name: :event_ts, type: String
           add_field name: :channel_type, type: String
 
           def tldr
             "type: #{type}; channel:#{channel}; raw_text:#{text}"
+          end
+
+          def thread_ts
+            return_nil?(@thread_ts) ? nil : @thread_ts
+          end
+
+          def parent_user_id
+            return_nil?(@parent_user_id) ? nil : @parent_user_id
+          end
+
+          private
+
+          def return_nil?(val)
+            JsonSchematize::EmptyValue === val || val.nil?
           end
         end
       end
